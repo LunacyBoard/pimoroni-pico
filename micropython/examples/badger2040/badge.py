08aa3aff349982feb3e3d10f0ff2e8e547e65af5
J D <jonathanbisset@Jonathans-MacBook-Pro.local>
2022-08-21 14:00:23 +0200

    Buttons A-C switch between badge profiles. Buttons UP/DOWN switch between logo & QR code

--- micropython/examples/badger2040/badge.py
+++ micropython/examples/badger2040/badge.py
@@ -1,6 +1,8 @@
 import time
 import badger2040
 import badger_os
+import qrcode
+import os
 
 # Global Constants
 WIDTH = badger2040.WIDTH
@@ -8,16 +10,16 @@ HEIGHT = badger2040.HEIGHT
 
 IMAGE_WIDTH = 104
 
-COMPANY_HEIGHT = 30
+COMPANY_HEIGHT = 40
 DETAILS_HEIGHT = 20
 NAME_HEIGHT = HEIGHT - COMPANY_HEIGHT - (DETAILS_HEIGHT * 2) - 2
 TEXT_WIDTH = WIDTH - IMAGE_WIDTH - 1
 
-COMPANY_TEXT_SIZE = 0.6
-DETAILS_TEXT_SIZE = 0.5
+COMPANY_TEXT_SIZE = 0.7
+DETAILS_TEXT_SIZE = 0.47
 
 LEFT_PADDING = 5
-NAME_PADDING = 20
+NAME_PADDING = 10
 DETAIL_SPACING = 10
 
 DEFAULT_TEXT = """mustelid inc
@@ -25,19 +27,24 @@ H. Badger
 RP2040
 2MB Flash
 E ink
-296x128px"""
+296x128px
+http://broken.com"""
 
-BADGE_IMAGE = bytearray(int(IMAGE_WIDTH * HEIGHT / 8))
+BADGE_STATES = 2
 
-try:
-    open("badge-image.bin", "rb").readinto(BADGE_IMAGE)
-except OSError:
+
+def load_image(filename="badge-image.bin"):
+    BADGE_IMAGE = bytearray(int(IMAGE_WIDTH * HEIGHT / 8))
     try:
-        import badge_image
-        BADGE_IMAGE = bytearray(badge_image.data())
-        del badge_image
-    except ImportError:
-        pass
+        open(filename, "rb").readinto(BADGE_IMAGE)
+    except OSError:
+        try:
+            import badge_image
+            BADGE_IMAGE = bytearray(badge_image.data())
+            del badge_image
+        except ImportError:
+            pass
+    return BADGE_IMAGE
 
 
 # ------------------------------
@@ -55,20 +62,51 @@ def truncatestring(text, text_size, width):
             return text
 
 
+
+def measure_qr_code(size, code):
+    w, h = code.get_size()
+    module_size = int(size / w)
+    return module_size * w, module_size
+
+
+def draw_qr_code(ox, oy, size, code):
+    size, module_size = measure_qr_code(size, code)
+    display.pen(15)
+    display.rectangle(ox, oy, size, size)
+    display.pen(0)
+    for x in range(size):
+        for y in range(size):
+            if code.get_module(x, y):
+                display.rectangle(ox + x * module_size, oy + y * module_size, module_size, module_size)
+                
+
 # ------------------------------
 #      Drawing functions
 # ------------------------------
 
 # Draw the badge, including user text
-def draw_badge():
+def draw_badge(img_state, BADGE_IMAGE, company, name, detail1_title, detail1_text, detail2_title, detail2_text, url):
     display.pen(0)
     display.clear()
 
-    # Draw badge image
-    display.image(BADGE_IMAGE, IMAGE_WIDTH, HEIGHT, WIDTH - IMAGE_WIDTH, 0)
+    # Draw badge image or qr code
+    if img_state == 0:
+        display.image(BADGE_IMAGE, IMAGE_WIDTH, HEIGHT, WIDTH - IMAGE_WIDTH, 0)
+    elif img_state == 1:
+        code = qrcode.QRCode()                
+        code.set_text(url)
+        size, _ = measure_qr_code(104, code)
+        print(size)
+        top = int((HEIGHT / 2) - (size / 2))
+        left = WIDTH - size
+        print(left)
+        display.pen(15)
+        display.rectangle(left - 3 , 0, size + 2 , HEIGHT)
+        draw_qr_code(left, top, 104, code)
+        
 
     # Draw a border around the image
-    display.pen(0)
+    display.pen(15)
     display.thickness(1)
     display.line(WIDTH - IMAGE_WIDTH, 0, WIDTH - 1, 0)
     display.line(WIDTH - IMAGE_WIDTH, 0, WIDTH - IMAGE_WIDTH, HEIGHT - 1)
@@ -81,19 +119,19 @@ def draw_badge():
 
     # Draw the company
     display.pen(15)  # Change this to 0 if a white background is used
-    display.font("serif")
-    display.thickness(3)
+    display.font("sans")
+    display.thickness(2) #default 3
     display.text(company, LEFT_PADDING, (COMPANY_HEIGHT // 2) + 1, COMPANY_TEXT_SIZE)
 
     # Draw a white background behind the name
     display.pen(15)
     display.thickness(1)
-    display.rectangle(1, COMPANY_HEIGHT + 1, TEXT_WIDTH, NAME_HEIGHT)
+    display.rectangle(0, COMPANY_HEIGHT + 1, TEXT_WIDTH + 1, NAME_HEIGHT + 1)
 
     # Draw the name, scaling it based on the available width
     display.pen(0)
     display.font("sans")
-    display.thickness(4)
+    display.thickness(2)
     name_size = 2.0  # A sensible starting scale
     while True:
         name_length = display.measure_text(name, name_size)
@@ -106,23 +144,23 @@ def draw_badge():
     # Draw a white backgrounds behind the details
     display.pen(15)
     display.thickness(1)
-    display.rectangle(1, HEIGHT - DETAILS_HEIGHT * 2, TEXT_WIDTH, DETAILS_HEIGHT - 1)
-    display.rectangle(1, HEIGHT - DETAILS_HEIGHT, TEXT_WIDTH, DETAILS_HEIGHT - 1)
+    display.rectangle(0, HEIGHT - DETAILS_HEIGHT * 2, TEXT_WIDTH + 1, DETAILS_HEIGHT )
+    display.rectangle(0, HEIGHT - DETAILS_HEIGHT, TEXT_WIDTH + 1, DETAILS_HEIGHT )
 
     # Draw the first detail's title and text
     display.pen(0)
     display.font("sans")
-    display.thickness(3)
+    display.thickness(1)
     name_length = display.measure_text(detail1_title, DETAILS_TEXT_SIZE)
     display.text(detail1_title, LEFT_PADDING, HEIGHT - ((DETAILS_HEIGHT * 3) // 2), DETAILS_TEXT_SIZE)
-    display.thickness(2)
+    display.thickness(1)
     display.text(detail1_text, 5 + name_length + DETAIL_SPACING, HEIGHT - ((DETAILS_HEIGHT * 3) // 2), DETAILS_TEXT_SIZE)
 
     # Draw the second detail's title and text
-    display.thickness(3)
+    display.thickness(1)
     name_length = display.measure_text(detail2_title, DETAILS_TEXT_SIZE)
     display.text(detail2_title, LEFT_PADDING, HEIGHT - (DETAILS_HEIGHT // 2), DETAILS_TEXT_SIZE)
-    display.thickness(2)
+    display.thickness(1)
     display.text(detail2_text, LEFT_PADDING + name_length + DETAIL_SPACING, HEIGHT - (DETAILS_HEIGHT // 2), DETAILS_TEXT_SIZE)
 
 
@@ -135,49 +173,65 @@ display = badger2040.Badger2040()
 display.led(128)
 display.update_speed(badger2040.UPDATE_NORMAL)
 
-# Open the badge file
-try:
-    badge = open("badge.txt", "r")
-except OSError:
-    with open("badge.txt", "w") as f:
-        f.write(DEFAULT_TEXT)
-        f.flush()
-    badge = open("badge.txt", "r")
 
-# Read in the next 6 lines
-company = badge.readline()        # "mustelid inc"
-name = badge.readline()           # "H. Badger"
-detail1_title = badge.readline()  # "RP2040"
-detail1_text = badge.readline()   # "2MB Flash"
-detail2_title = badge.readline()  # "E ink"
-detail2_text = badge.readline()   # "296x128px"
+def load_data(filename="badge.txt"):
+    # Open the badge file
+    try:
+        badge = open(filename, "r")
+    except OSError:
+        with open(filename, "w") as f:
+            f.write(DEFAULT_TEXT)
+            f.flush()
+        badge = open(filename, "r")
+    return ([badge.readline(), badge.readline(), badge.readline(), badge.readline(), badge.readline(), badge.readline(), badge.readline()])
 
-# Truncate all of the text (except for the name as that is scaled)
-company = truncatestring(company, COMPANY_TEXT_SIZE, TEXT_WIDTH)
+def format_data_lines(datalines):
+    datalines[0] = truncatestring(datalines[0], COMPANY_TEXT_SIZE, TEXT_WIDTH)
+    get_badge_data[2] = truncatestring(get_badge_data[2], DETAILS_TEXT_SIZE, TEXT_WIDTH)
+    get_badge_data[3] = truncatestring(get_badge_data[3], DETAILS_TEXT_SIZE,
+                              TEXT_WIDTH - DETAIL_SPACING - display.measure_text(get_badge_data[2], DETAILS_TEXT_SIZE))
 
-detail1_title = truncatestring(detail1_title, DETAILS_TEXT_SIZE, TEXT_WIDTH)
-detail1_text = truncatestring(detail1_text, DETAILS_TEXT_SIZE,
-                              TEXT_WIDTH - DETAIL_SPACING - display.measure_text(detail1_title, DETAILS_TEXT_SIZE))
-
-detail2_title = truncatestring(detail2_title, DETAILS_TEXT_SIZE, TEXT_WIDTH)
-detail2_text = truncatestring(detail2_text, DETAILS_TEXT_SIZE,
-                              TEXT_WIDTH - DETAIL_SPACING - display.measure_text(detail2_title, DETAILS_TEXT_SIZE))
+    get_badge_data[4] = truncatestring(get_badge_data[4], DETAILS_TEXT_SIZE, TEXT_WIDTH)
+    get_badge_data[5] = truncatestring(get_badge_data[5], DETAILS_TEXT_SIZE,
+                              TEXT_WIDTH - DETAIL_SPACING - display.measure_text(get_badge_data[4], DETAILS_TEXT_SIZE))
+    return datalines
 
 
 # ------------------------------
 #       Main program
 # ------------------------------
-
-draw_badge()
+badge_state = 0
+b_text_file = "badgeA.txt"
+b_image_file = "badge-imageA.bin"
 
 while True:
-    if display.pressed(badger2040.BUTTON_A) or display.pressed(badger2040.BUTTON_B) or display.pressed(badger2040.BUTTON_C) or display.pressed(badger2040.BUTTON_UP) or display.pressed(badger2040.BUTTON_DOWN):
-        badger_os.warning(display, "To change the text, connect Badger2040 to a PC, load up Thonny, and modify badge.txt")
-        time.sleep(4)
+    # Buttons A-C switch between different cards
+    if display.pressed(badger2040.BUTTON_A):
+        b_text_file = "badgeA.txt"
+        b_image_file = "badge-imageA.bin"
+        
+    if display.pressed(badger2040.BUTTON_B):
+        b_text_file = "badgeB.txt"
+        b_image_file = "badge-imageB.bin"
+        
+    if display.pressed(badger2040.BUTTON_C):
+        b_text_file = "badgeC.txt"
+        b_image_file = "badge-imageC.bin"
+        
+    # UP & DOWN switch between image displayed - currently the image or the QR code from the last row of text    
+    if display.pressed(badger2040.BUTTON_UP):
+        badge_state = badge_state -1 if badge_state > 0 else BADGE_STATES - 1
+        
+    if display.pressed(badger2040.BUTTON_DOWN):
+        badge_state = badge_state + 1 if badge_state < BADGE_STATES - 1  else 0 
 
-        draw_badge()
+    get_badge_data = load_data(filename = b_text_file)
+    get_badge_image = load_image(filename = b_image_file)
+    get_badge_data_f = format_data_lines(get_badge_data)
+    draw_badge(badge_state, get_badge_image, *get_badge_data_f)
 
     display.update()
 
     # If on battery, halt the Badger to save power, it will wake up if any of the front buttons are pressed
     display.halt()
+
